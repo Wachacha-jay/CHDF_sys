d78736a321272c71b13e71ed045e34c4db19b91c
Wachacha-jay <jameswachacha@gmail.com>
2026-09-11 18:28:10 +0300

    logo setting

--- server/index.ts
+++ server/index.ts
@@ -20,7 +20,8 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 // Serve uploaded files
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
